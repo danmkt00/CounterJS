19b1d0e427943409454bd40a5ef2f3374ac81682
DanielMikitiuk <dani4ku@gmail.com>
2025-02-25 14:33:01 +0100

    add working counter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('Hello, world!');
+const display = document.getElementById('display');
+const plusBtn = document.getElementById('plus-btn');
+const resetBtn = document.getElementById('reset-btn');
+const minusBtn = document.getElementById('minus-btn');
+
+data = 0;
+
+plusBtn.addEventListener('click', () => {
+    data++;
+    display.innerText = data;
+});
+
+resetBtn.addEventListener('click', () => {
+    data = 0;
+    display.innerText = data;
+});
+
+minusBtn.addEventListener('click', () => {
+    data--;
+    display.innerText = data;
+});
